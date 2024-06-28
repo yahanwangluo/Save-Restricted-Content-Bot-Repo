a0d2386fc3ab9cfae7ed2ae9ff1d149fa80f4550
metaidc.net <73825775+yahanwangluo@users.noreply.github.com>
2024-06-28 15:33:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+BOT_TOKEN = getenv("BOT_TOKEN", "7464232630:AAFKubeR667vBY_4HFQNs0oT6wBfEd87lOQ")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6232350467").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+LOG_GROUP = getenv("LOG_GROUP", "-1002179112500")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002179112500"))
